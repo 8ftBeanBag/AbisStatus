e95ffc931551a7e109ab11e7437c50b389cc42e4
Abi Hendrick <hello@abigailhendrick.com>
2023-08-16 20:06:49 -0400

    Made the thing

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,113 +1,119 @@
 import Image from 'next/image'
+import key from "./weatherKey"
+import axios from "axios"
+import queryString from 'query-string'
+import moment from 'moment'
+import Icon from '@mdi/react'
+import {mdiWifi} from "@mdi/js"
 
-export default function Home() {
+import type { Metadata } from 'next'
+ 
+export const metadata: Metadata = {
+  title: "Abi's Status",
+  description: 'The status of Abi',
+}
+
+export default async function Home() {
+  const weather:string|Boolean = await getWeather();
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+      <p className='text-5xl text-white text-center font-dancing'>Abi&apos;s Status</p>
+      {weather === "error" ? <p>Wifi Status N/A</p> : <Icon path={mdiWifi} color={weather ? "red" : "green"} size={1}></Icon>}
+      <Status weather={weather}></Status>
     </main>
   )
+}
+
+type StatusProps = {
+  weather: string | Boolean
+}
+async function Status(props:StatusProps){
+  let startTime:Date = new Date();
+  startTime.setHours(8,0,0,0);
+  const endTime:Date = new Date();
+  endTime.setHours(18,0,0,0);
+
+  if((+startTime - Date.now() < 0) && (+endTime - Date.now() > 0) && !props.weather){
+    return (
+      <p>     
+        <Image alt="Abi headshot" width="200" height="200" src="/cropped_headshot.png"></Image>
+        Abi should be available 🤔
+      </p>
+    )
+  }
+  else if(+startTime - Date.now() > 0){
+    return (
+      <div className='text-center flex flex-col items-center'>
+        <p>Abi hasn&apos;t had her coffee yet. <br/> You probably don&apos;t want to contact her.</p>
+        <Image alt="Coffee" width="200" height="200" src="/coffee.png"></Image>
+      </div>
+    )
+  }
+  else if(+endTime - Date.now() < 0){
+    return (
+      <div className='text-center flex flex-col items-center'>
+        <p>Abi is fighting crime in Gotham with her evening. <br/> You can only reach her through the batmobile&apos;s number.</p>
+        <Image alt="Bat speech bubble" width="200" height="200" src="/bat.png"></Image>
+      </div>
+    )
+  }
+  else if(props.weather){
+    return (
+      <div className='text-center flex flex-col items-center'>
+        <p className='font-inter'>Storms are probably cutting out Abi&apos;s internet.</p>
+        <span className='text-xs text-secondary'>Florida <em>hates</em> Abi&apos;s Internet.</span>
+        <Image alt="Thunderstorm" width="200" height="200" src="/ts.png"></Image>
+      </div>
+    )
+  }
+  return <p>Oops no Abi status today</p>
+}
+
+async function getWeather(){
+  // set the Timelines GET endpoint as the target URL
+  const getTimelineURL = "https://api.tomorrow.io/v4/timelines";
+
+  // get your key from app.tomorrow.io/development/keys
+  const apikey = key;
+
+  // pick the location, as a latlong pair
+  let location = [29.79044554889197, -82.5563356034591];
+
+  // list the fields
+  const fields = ["weatherCode"];
+
+  // choose the unit system, either metric or imperial
+  const units = "imperial";
+
+  // set the timesteps, like "current", "1h" and "1d"
+  const timesteps = ["current"];
+
+  // configure the time frame up to 6 hours back and 15 days out
+  const now = moment.utc();
+  const startTime = moment.utc(now).add(0, "minutes").toISOString();
+  const endTime = moment.utc(now).add(1, "days").toISOString();
+
+  // specify the timezone, using standard IANA timezone format
+  const timezone = "America/New_York";
+
+  // request the timelines with all the query string parameters as options
+  const getTimelineParameters =  queryString.stringify({
+      apikey,
+      location,
+      fields,
+      units,
+      timesteps,
+      startTime,
+      endTime,
+      timezone,
+  }, {arrayFormat: "comma"});
+
+  try{
+    const result = await axios.get(getTimelineURL + "?" + getTimelineParameters)
+    return result.data.data.timelines[0].intervals[0].values.weatherCode===8000;
+  }
+  catch (error) {
+    return "Error";
+  }
 }
